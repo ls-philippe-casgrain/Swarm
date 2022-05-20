9c0b48e25e71417170f806bd1542d3ced665de12
Philippe Casgrain <philippe.casgrain@lightspeedhq.com>
2022-05-20 12:40:53 -0400

    Initial Commit

---
+++ Swarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swarm
+//
+//  Created by Philippe Casgrain on 2022-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swarm/SwarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwarmApp.swift
+//  Swarm
+//
+//  Created by Philippe Casgrain on 2022-05-20.
+//
+
+import SwiftUI
+
+@main
+struct SwarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
